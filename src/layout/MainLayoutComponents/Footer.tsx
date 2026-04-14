9969da726cf0d07463ef663040390b37ffb75693
Jurgen <jbrettschneider03@gmail.com>
2026-04-14 14:05:26 +0200

    Iniital commit

---
+++ src/layout/MainLayoutComponents/Footer.tsx
@@ -0,0 +1,16 @@
+import styles from "./css/Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.f}>
+      <div className={styles.footerContent}>
+        <p>&copy; 2025 Barnes Kapital. All rights reserved.</p>
+        <p>Private & Discreet | Long-Term Value</p>
+        <p>Contact: info@barneskapital.com</p>
+        <p>This website uses fullPage.js</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
